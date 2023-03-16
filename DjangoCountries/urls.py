5c5eda543219693d52f7672f3adf8c4d6ae22c90
Foxycrime <99174584+Foxycrime@users.noreply.github.com>
2023-03-17 00:04:04 +0300

    Update urls.py

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import *
+from django.views import *
+from MainApp import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.about),
+    # path('about', include (MainApp.url),
+    path('countries-list/', views.list),
+    path('country/<str:country>', views.country_page),
+    path('country/countries-list/', views.list),
+    path('languages_list/', views.languages_list),
+    path('countries-list/<str:language>', views.country),
+    path('language/', views.country),
+    # path('countries/', views.post_list),
 ]
